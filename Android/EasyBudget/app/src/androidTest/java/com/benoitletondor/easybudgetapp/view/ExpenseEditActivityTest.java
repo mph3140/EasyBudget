5e7489d2057757b88b094926f8f6b964a9ed13d2
Matthew Hanna <mph3140@gmail.com>
2018-10-15 18:20:17 -0400

    Commit for Extra files

--- Android/EasyBudget/app/src/androidTest/java/com/benoitletondor/easybudgetapp/view/ExpenseEditActivityTest.java
+++ Android/EasyBudget/app/src/androidTest/java/com/benoitletondor/easybudgetapp/view/ExpenseEditActivityTest.java
@@ -8,10 +8,11 @@ class ExpenseEditActivityTest extends ExpenseEditActivity{
     @Test
     public void Should_edit_expense()
     {
+        ExpenseEditActivity sut = new ExpenseEditActivity();
         amountEditText.setText("50.00");
         String amount = amountEditText.getText().toString();
         double value = 50.00;
-        double value1 = Double.parseDouble(expense.getAmount(amount));
+        double value1 = Double.parseDouble(expense.getAmount());
        // expenseToSave = expense;
        // expenseToSave.setTitle(descriptionEditText.getText().toString());
        // expenseToSave.setAmount(isRevenue ? -value : value);
