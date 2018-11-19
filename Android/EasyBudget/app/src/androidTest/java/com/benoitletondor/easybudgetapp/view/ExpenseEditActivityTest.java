57f6e0dde1b70ef5a7d9bd241dd2783f16744eca
Matthew Hanna <mph3140@gmail.com>
2018-11-19 18:58:00 -0500

    Commit before pull

--- Android/EasyBudget/app/src/androidTest/java/com/benoitletondor/easybudgetapp/view/ExpenseEditActivityTest.java
+++ Android/EasyBudget/app/src/androidTest/java/com/benoitletondor/easybudgetapp/view/ExpenseEditActivityTest.java
@@ -5,17 +5,17 @@ import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ExpenseEditActivityTest extends ExpenseEditActivity{
-    @Test
-    public void Should_edit_expense()
-    {
-        ExpenseEditActivity sut = new ExpenseEditActivity();
-        amountEditText.setText("50.00");
-        String amount = amountEditText.getText().toString();
-        double value = 50.00;
-        double value1 = Double.parseDouble(expense.getAmount());
-       // expenseToSave = expense;
-       // expenseToSave.setTitle(descriptionEditText.getText().toString());
-       // expenseToSave.setAmount(isRevenue ? -value : value);
-       // expenseToSave.setDate(date);
-    }
+//    @Test
+//    public void Should_edit_expense()
+//    {
+//        ExpenseEditActivity sut = new ExpenseEditActivity();
+//        amountEditText.setText("50.00");
+//        String amount = amountEditText.getText().toString();
+//        double value = 50.00;
+//        double value1 = Double.parseDouble(expense.getAmount());
+//       // expenseToSave = expense;
+//       // expenseToSave.setTitle(descriptionEditText.getText().toString());
+//       // expenseToSave.setAmount(isRevenue ? -value : value);
+//       // expenseToSave.setDate(date);
+//    }
 }
